4586070f019ea8e9e62fb87d4de143291eef75af
Pietro <pietrocariola@gmail.com>
2026-05-07 11:06:47 -0300

    iperf.sh multi stas

--- iperf.sh
+++ iperf.sh
@@ -1,20 +1,46 @@
 #!/usr/bin/env bash
 
-STAS=(
-  "192.168.1.3"
-  "192.168.1.4"
-)
+# add servers here
+SERVERS=("192.168.1.4" "192.168.1.5")
+BITRATES=("10M" "10M")
+LENGTHS=("1400" "1400")
+PROTOCOLS=("UDP" "TCP") 
 
-RATE_PER_STA="10M"   # start low; increase later
-PKT_LEN="1200"
+# clean up group processes (and childs) when Ctrl+C
+cleanup() {
+    echo
+    echo "Stopping all iperf3 processes..."
+    kill 0
+    exit 0
+}
 
-for STA_IP in "${STAS[@]}"; do
-  (
+trap cleanup SIGINT
+
+echo "Starting iperf3 tests in parallel"
+echo "Press Ctrl+C to stop"
+echo
+
+for i in "${!SERVERS[@]}"; do
+
+    SERVER="${SERVERS[$i]}"
+    BITRATE="${BITRATES[$i]}"
+    LENGTH="${LENGTHS[$i]}"
+    PROTOCOL="${PROTOCOLS[$i]}"
+
+    echo "Starting iperf3 for $SERVER"
+    echo "Bitrate: "$BITRATE"bits/s"
+    echo "Packet length: "$LENGTH"bytes"
+    echo "Protocol: $PROTOCOL"
+    echo
+
     while true; do
-      iperf3 -c "$STA_IP" -u -b "$RATE_PER_STA" -l "$PKT_LEN" -t 60
-      sleep 1
-    done
-  ) &
+        if [ "$PROTOCOL" = "UDP" ]; then
+            iperf3 -u -c "$SERVER" -b "$BITRATE" -l "$LENGTH"
+        else
+            iperf3 -c "$SERVER" -b "$BITRATE" -l "$LENGTH"        
+        fi         
+        sleep 1
+    done &
 done
 
 wait
