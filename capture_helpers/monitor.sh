a347f70ced9ba528fbf7d7bf7c99e44f54b5545c
Pietro <pietrocariola@gmail.com>
2026-05-29 12:07:02 -0300

    cleaning repo | gitignore update

--- capture_helpers/monitor.sh
+++
@@ -1,6 +0,0 @@
-wpa_passphrase "NETGEAR93-5G" "shinycream130" > config
-sudo wpa_supplicant -B -i wlp8s0 -c config
-sudo dhclient wlp8s0
-sudo airmon-ng start wlp8s0
-sudo iw dev wlp8s0 set channel 44 80MHz
-sudo tshark -i wlp8s0 -w capture.pcapng
